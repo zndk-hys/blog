602c4c51aabac193b99b46a978bae256168837fa
zndk-hys <hysryt+zndk@gmail.com>
2025-10-19 22:25:08 +0900

    ブログ詳細にdynamic = force-static 追加

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 
+export const dynamic = 'force-static';
 export const revalidate = false;
 
 type Props = {
