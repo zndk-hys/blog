caaa7361522f00726708e7f9e1e67d5a9ee6a403
zndk-hys <hysryt+zndk@gmail.com>
2025-10-19 20:57:00 +0900

    webhook handler

---
+++ app/blog/api/webhook/microcms/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import crypto from "crypto";
+import { revalidatePath } from "next/cache";
+
+export async function POST(request: NextRequest) {
+  const body = await request.text();
+
+  const signature = request.headers.get('x-microcms-signature');
+  if (!signature) {
+    console.log('not found x-microcms-signature header');
+    return NextResponse.json({error: 'Invalid signature.'}, {status: 401});
+  }
+  console.log('x-microcms-signature: ' + signature);
+
+  const secret = process.env.MICROCMS_WEBHOOK_SECRET;
+  if (!secret) {
+    console.log('not found SECRET env: ' + signature);
+    return NextResponse.json({error: 'Server error'}, {status: 500});
+  }
+
+  const expectedSignature = crypto
+    .createHmac('sha256', secret)
+    .update(body)
+    .digest('hex');
+
+  const sigBuf = Buffer.from(signature, 'hex');
+  const expBuf = Buffer.from(expectedSignature, 'hex');
+
+  if (sigBuf.length !== expBuf.length || !crypto.timingSafeEqual(sigBuf, expBuf)) {
+    console.log('exp: '+ expectedSignature);
+    console.log('not match signature');
+    return NextResponse.json({error: 'Invalid signature.'}, {status: 401});
+  }
+
+  revalidatePath('/blog');
+  
+  return NextResponse.json({ok: true}, {status: 200});
+}
